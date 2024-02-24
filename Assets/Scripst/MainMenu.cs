083fa0db9566abb8dec0e9583a03859b18c396bb
dubuduck <jellysky61@gmail.com>
2024-02-23 22:25:56 -0300

    Add Main Menu

---
+++ Assets/Scripst/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    public void Scene01()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Scene02()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
+
